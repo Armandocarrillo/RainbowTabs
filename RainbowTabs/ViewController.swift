c2acc73791eb515f1f1509f4cb192c24751eb03f
Armando Carrillo <ingcompuarmando@gmail.com>
2020-05-05 15:27:19 -0500

    Initial Commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Armando Carrillo on 05/05/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
